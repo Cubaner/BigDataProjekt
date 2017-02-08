cc2964213b7bba0bb37a6876ce519b746e8ee7d4
Gemba69 <bluhn@web.de>
2017-02-08 15:47:47 +0100

    further bugfixes

--- bigdata-nfl-hbase-interface/src/main/java/de/fhm/bigdata/projekt/hbase/HBaseConnectionManager.java
+++ bigdata-nfl-hbase-interface/src/main/java/de/fhm/bigdata/projekt/hbase/HBaseConnectionManager.java
@@ -34,7 +34,7 @@ public class HBaseConnectionManager {
 	private static Configuration config;
 	
 	private static String TABLE_TEST = "test2";
-	private static String TABLE_TEAMS = "";
+	private static String TABLE_TEAMS = "teams";
 	private static String TABLE_HASHTAGS = "hashtags";
 	
 	private static final byte[] COLUMN_FAMILY = Bytes.toBytes("");
@@ -45,13 +45,23 @@ public class HBaseConnectionManager {
 	private static final byte[] COLUMN_HASHTAGS_COUNTER = Bytes.toBytes("counter");
 	private static final byte[] COLUMN_HASHTAGS_TIMESTAMP = Bytes.toBytes("timestamp");
 	
+	private static final byte[] COLUMN_FAMILY_TEAMS = Bytes.toBytes("hashtag_teams");
+	private static final byte[] COLUMN_TEAMS_NAME = Bytes.toBytes("name");
+	private static final byte[] COLUMN_TEAMS_RANK = Bytes.toBytes("rank");
+	private static final byte[] COLUMN_TEAMS_ID = Bytes.toBytes("id");
+	private static final byte[] COLUMN_TEAMS_DIVISION = Bytes.toBytes("division");
+	private static final byte[] COLUMN_TEAMS_FORECAST = Bytes.toBytes("forecast");
+	private static final byte[] COLUMN_TEAMS_SYNONYM = Bytes.toBytes("synonym");
+	
 	public HBaseConnectionManager() {
         try {
             config = HBaseConfiguration.create();
-            config.clear();
+            
+            /*config.clear();
             config.set("hbase.zookeeper.quorum", "172.17.0.2");
             config.set("hbase.zookeeper.property.clientPort","2181");
             config.set("hbase.master", "172.17.0.2:60000");
+            */
             //HBaseConfiguration config = HBaseConfiguration.create();
             //config.set("hbase.zookeeper.quorum", "localhost");  // Here we are running zookeeper locally
             HBaseAdmin.checkHBaseAvailable(config);
@@ -74,12 +84,63 @@ public class HBaseConnectionManager {
 		}
 	}
 	
+	public ArrayList<NFLTeam> getTeamRating() {
+		ArrayList<NFLTeam> resultList = new ArrayList<NFLTeam>();
+		ResultScanner rs = null;
+		Result res = null;
+		String hashtag = null;
+		int rank;
+		int id;
+		String division;
+		String forecast;
+		String[] synonym;
+		NFLTeam result = null;
+		try {
+			HTable table = new HTable(config, TABLE_TEAMS);
+			Scan scan = new Scan();
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_NAME);
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_RANK);
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_ID);
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_DIVISION);
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_FORECAST);
+			scan.addColumn(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_SYNONYM);
+			rs = table.getScanner(scan);
+			while ((res = rs.next()) != null) {
+
+				byte[] rohHashtag = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_NAME);
+				byte[] rohRank = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_RANK);
+				byte[] rohId = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_ID);
+				byte[] rohDivision = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_DIVISION);
+				byte[] rohForecast = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_FORECAST);
+				byte[] rohSynonym = res.getValue(COLUMN_FAMILY_TEAMS, COLUMN_TEAMS_SYNONYM);
+
+				hashtag = Bytes.toString(rohHashtag);
+				rank = Bytes.toInt(rohRank);
+				id = Bytes.toInt(rohId);
+				division = Bytes.toString(rohDivision);
+				forecast = Bytes.toString(rohForecast);
+				synonym = Bytes.toString(rohSynonym).split(";");
+				
+				result = new NFLTeam (hashtag, rank, id, division, forecast, synonym);
+				if (!resultList.contains(result)) {
+					resultList.add(result);
+				}
+			}
+		} catch (IOException e) {	
+			System.out.println("Exception occured in retrieving data");
+		} finally {
+			rs.close();
+		}
+		Collections.sort(resultList, NFLTeam.getNFLTeamByRank());
+		return resultList;
+	}
+	
 	public ArrayList<Hashtag> getTopHashtags() {
 		ArrayList<Hashtag> resultList = new ArrayList<Hashtag>();
 		ResultScanner rs = null;
 		Result res = null;
 		String hashtag = null;
-		String counter;
+		int counter;
 		Hashtag result = null;
 		try {
 			HTable table = new HTable(config, TABLE_HASHTAGS);
@@ -93,9 +154,9 @@ public class HBaseConnectionManager {
 				byte[] rohCounter = res.getValue(COLUMN_FAMILY_HASHTAGS, COLUMN_HASHTAGS_COUNTER);
 
 				hashtag = Bytes.toString(rohHashtag);
-				counter = Bytes.toString(rohCounter);
+				counter = Bytes.toInt(rohCounter);
 				result = new Hashtag (hashtag, counter);
-				if (!resultList.contains(result)) {
+				if (!resultList.contains(result) && !hashtag.isEmpty()) {
 					resultList.add(result);
 				}
 			}
